97a5b35ee7a5264ed5690400356b9b2b8f07b0ed
Nicolas Naso <nnaso@eiwa.ag>
2025-04-11 14:09:00 -0300

    fix(hero-form): change alignment input for select

--- src/app/features/heroes/components/hero-form/hero-form.component.ts
+++ src/app/features/heroes/components/hero-form/hero-form.component.ts
@@ -14,8 +14,10 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { UppercaseDirective } from '../../../../shared/directives';
 import { Hero } from '../../models';
 
@@ -28,6 +30,8 @@ import { Hero } from '../../models';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatSelectModule,
+    MatOptionModule,
     UppercaseDirective,
   ],
   templateUrl: './hero-form.component.html',
