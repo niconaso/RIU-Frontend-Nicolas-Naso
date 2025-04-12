cb45a03052192daa77b185be09364e87c03819ed
Nicolas Naso <nnaso@eiwa.ag>
2025-04-12 10:47:52 -0300

    fix(loading): remove unused count variable

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -3,20 +3,15 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class LoadingService {
-  #count = 0;
   #isLoading = new BehaviorSubject<boolean>(false);
+
   loading$ = this.#isLoading.asObservable();
 
   public show() {
-    this.#count++;
     this.#isLoading.next(true);
   }
 
   public hide() {
-    this.#count--;
-    if (this.#count <= 0) {
-      this.#isLoading.next(false);
-      this.#count = 0;
-    }
+    this.#isLoading.next(false);
   }
 }
